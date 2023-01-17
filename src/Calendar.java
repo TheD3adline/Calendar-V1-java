46ae545dd13c2f81948ff80d8d34d47090c41baf
MichaelFessler-Work <michaelfessler@gmx.at>
2023-01-17 10:35:17 +0100

    Added commentary

--- src/Calendar.java
+++ src/Calendar.java
@@ -3,30 +3,41 @@
     Date: 2023/01/10
     Version: 0.1
     Description:
-            Class with calendar specific methods und functions.
+            Calendar specific methods und functions.
  */
 public class Calendar {
+
+    // String Array for the output of the months
     private static final String[] months = {"January", "February", "March", "April", "May", "June", "July",
                                             "August", "September", "October", "November", "December"};
 
+    /**
+     * To read given String from textField1, check validity of input, and reformat data according to the task
+     * @param input given String from textField1
+     * @return String, the reformatted data or error message
+     */
     public static String getInput(String input) {
         String r = "Invalid Input";
         StringBuilder outputFormat = new StringBuilder();
+        //Checks validity of input
         if(input.length() >= 7) {
             if(isNumeric(input.substring(0, 2)) && isNumeric(input.substring(3, 5)) &&
                     isNumeric(input.substring(6)) && ((input.charAt(2) == '.') && (input.charAt(5) == '.'))) {
+                //Reformatting of the data
                 int day = Integer.parseInt(input.substring(0, 2));
                 int month = Integer.parseInt(input.substring(3, 5));
                 int year = Integer.parseInt(input.substring(6));
+                //Checks if given data is in accordance with the Gregorian calendar by calling dateIsValid() sub-method
                 if(dateIsValid(day, month, year)) {
                     if(day < 10)
                         outputFormat.append("0").append(day).append(".")
                                     .append(months[month - 1]).append(".").append(year);
                     else
                         outputFormat.append(day).append(".").append(months[month - 1]).append(".").append(year);
-                    if(year < 0) {
+                    if(year < 0) { //If given year is < 0 appends B.C. to the output
                         outputFormat.append(" B.C.");
                     }
+                    //Calls isLeapYear() sub-method to mark a possible leap year with the ":" for the GUI logic
                     if(isLeapYear(year))
                         outputFormat.append(":");
                     r = String.valueOf(outputFormat);
@@ -37,6 +48,11 @@ public class Calendar {
         return r;
     }
 
+    /**
+     * Checks if given String is numeric
+     * @param str given String
+     * @return boolean true if String is numeric, false if null or not numeric
+     */
     private static boolean isNumeric(String str) {
         if(str == null)
             return false;
@@ -48,16 +64,29 @@ public class Calendar {
         return true;
     }
 
+    /**
+     * Method to check if given date is in accordance with the Gregorian calendar, days per month, and 1 to 12 months
+     * @param day given int for days
+     * @param month given int for months
+     * @param year given int for years
+     * @return boolean true if given date is in accordance with Gregorian calendar, false if not
+     */
     private static boolean dateIsValid(int day, int month, int year) {
         if(((month == 1) || (month == 3) || (month == 5) || (month == 7) || (month == 8) ||
                 (month == 10) || (month == 12)) && (day >= 1 && day <= 31))
             return true;
+        //Calls isLeapYear() sub-method to check if month 2, February, can have 29 days if leap year or 28 if not
         else if(((month == 2) && (isLeapYear(year) && (day >= 1 && day <= 29))) ||
                 ((month == 2) && (!isLeapYear(year) && (day >= 1 && day <= 28))))
             return true;
         else return ((month == 4) || (month == 6) || (month == 9) || (month == 11)) && (day >= 1 && day <= 30);
     }
 
+    /**
+     * Method to check if given year is a leap year or not according to the definition of the Gregorian calendar
+     * @param year given year
+     * @return boolean true if given year is a leap year, false if not
+     */
     private static boolean isLeapYear(int year) {
         if((year % 4 == 0) && (year % 100 != 0))
             return true;
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -3,35 +3,55 @@
     Date: 2023/01/10
     Version: 0.1
     Description:
-            Class with GUI elements.
+            GUI elements.
  */
 import javax.swing.*;
 public class MainFrame extends JFrame {
-    private JTextField textField1;
-    private JTextField textField2;
-    private JButton OKButton;
-    private JPanel mainPanel;
-    private JCheckBox isLeapYearCheckBox;
-    private JButton clearButton;
 
+    //The different GUI elements
+    private JTextField textField1;          //Input text field
+    private JTextField textField2;          //Output text field
+    private JButton OKButton;               //Button to confirm input and feed it to the following logic
+    private JPanel mainPanel;               //The main window
+    private JCheckBox isLeapYearCheckBox;   //The checkbox to display a leap year
+    private JButton clearButton;            //Button to clear input and reset the logic
+
+    /**
+     * Constructor for the main window with ActionListeners for the buttons
+     */
     public MainFrame() {
         setContentPane(mainPanel);
         setTitle("Calendar");
         setSize(350, 250);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         isLeapYearCheckBox.setEnabled(false);
+        /*
+            On button press reads content from textField1 to the String date and
+            hands it to the logic in Calendar.java for processing of the given data.
+         */
         OKButton.addActionListener(e -> {
             String date = Calendar.getInput(textField1.getText());
             if(date.contains(":")) {
+                /*
+                    If the returned String contains a ":" it was marked as a leap year,
+                    the mark is removed and the checkbox is selected.
+                 */
                 date = date.replace(":", "");
                 isLeapYearCheckBox.setSelected(true);
             }
+            /*
+                The reformatted date (or the error message if the given format was invalid)
+                will now be displayed in textField2, then textField1 and OKButton will be disabled.
+             */
             textField2.setText(date);
             textField1.setEditable(false);
             OKButton.setEnabled(false);
 
         });
 
+        /*
+            ActionListener on clearButton to reset all input fields, buttons, and the logic.
+         */
         clearButton.addActionListener(e -> {
             textField1.setText("");
             textField2.setText("");
